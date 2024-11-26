fa4b1b3a2b321cf4e00502fd907c6a68acf0db19
manojseetaram <manojseetaram.artytech@gmail.com>
2024-11-26 18:21:17 +0530

    first commit

---
+++ project-1/vitepractice/src/App.jsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='p-4 bg-pink-600 text-3xl'>Hey Manoj</h1>
+      
+    </>
+  )
+}
+
+export default App
